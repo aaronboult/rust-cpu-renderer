2e766e13a0e132efcc269f7b78e43136f0a941ba
Aaron Boult <aaronboult25@gmail.com>
2021-04-17 02:33:32 +0100

    Removed generics where types could be hard coded, added 3D rotated projection and scaling and added Transform struct

--- rust/src/simulator/renderer.rs
+++ rust/src/simulator/renderer.rs
@@ -1,24 +1,23 @@
 pub mod geometry;
-use geometry::{Vector, Matrix, Vector2D, Vector3D};
+use geometry::{Matrix, Vector2D, Vector3D};
 
-use super::{Transform};
-
+#[allow(dead_code)]
 pub enum RenderMode {
-    R_2D,
-    R_3D
+    R2D,
+    R3D
 }
 
-pub struct Camera<T> where T: Transform {
-    transform: T,
+pub struct Camera {
+    transform: Transform,
     view_plane_top_left: Vector2D,
     view_plane_bottom_right: Vector2D,
     view_plane_z: f32
 }
 
-impl<T> Camera<T> where T: Transform {
+impl Camera {
     pub fn new() -> Self {
         Self {
-            transform: T::default(),
+            transform: Transform::default(),
             view_plane_top_left: Vector2D::new(-1.0, 1.0),
             view_plane_bottom_right: Vector2D::new(1.0, -1.0),
             view_plane_z: -1.0
@@ -26,48 +25,46 @@ impl<T> Camera<T> where T: Transform {
     }
 }
 
-pub struct Renderer<T> where T: Transform {
+pub struct Renderer {
     mode: RenderMode,
-    camera: Camera<T>,
-    scale: f32
+    camera: Camera
 }
 
-impl<T> Renderer<T> where T: Transform {
+impl Renderer {
     pub fn new(mode: RenderMode) -> Self {
         Self {
             mode,
-            camera: Camera::new(),
-            scale: 1.0
+            camera: Camera::new()
         }
     }
 
-    pub fn project_to_screen<U>(&self, transform: T, point: U) -> Vector2D where T: Transform, U: Vector {
+    pub fn project_to_screen(&self, transform: &Transform, vertex: &Vector3D, window_size: (u32, u32)) -> (i32, i32) {
         match self.mode {
-            RenderMode::R_2D => {
-                Vector2D::ZERO
+            RenderMode::R2D => {
+                (-1, -1)
             },
-            RenderMode::R_3D => {
-                self.calculate_3d_projection(transform, point)
+            RenderMode::R3D => {
+                self.calculate_3d_projection(transform, vertex, window_size)
             }
         }
     }
 
-    fn calculate_3d_projection<U>(&self, transform: T, point: U) -> Vector2D where T: Transform, U: Vector {
+    fn calculate_3d_projection(&self, transform: &Transform, vertex: &Vector3D, window_size: (u32, u32)) -> (i32, i32) {
 
         let point = Vector3D::new(
-            point.get_x(),
-            point.get_y(),
-            point.get_z()
+            vertex.x,
+            vertex.y,
+            vertex.z
         );
 
-        let rotation = transform.get_rotation();
+        let rotation = transform.rotation * (std::f32::consts::PI / 180.0);
     
         // scale the initial point
-        let projected_point = Matrix::from(point * transform.get_scale());
+        let projected_point = Matrix::from(point * transform.scale);
 
         let x_rotation_matrix = Matrix::from_vec(3, 3, vec![
             1.0, 0.0, 0.0,
-            0.0, rotation.x.cos(), -rotation.y.cos(),
+            0.0, rotation.x.cos(), -rotation.x.sin(),
             0.0, rotation.x.sin(), rotation.x.cos()
         ]);
 
@@ -88,22 +85,41 @@ impl<T> Renderer<T> where T: Transform {
         // multiplying rotation matrix with 3d column vector 
         // produces another 3d column vector
         let rotated_projection = Vector3D::from(rotation_matrix * projected_point)
-            + transform.get_position()
-            - self.camera.transform.get_position();
+            + transform.position
+            - self.camera.transform.position;
 
         let recording_screen_size = self.camera.view_plane_bottom_right
             - self.camera.view_plane_top_left;
-        
-        if rotated_projection.z > 0.0 {
-            let resolution_x = 512.0;
-            let resolution_y = 512.0;
-            return Vector2D::new(
-                (rotated_projection.x * resolution_x) / (rotated_projection.z / recording_screen_size.x) * self.camera.view_plane_z,
-                (rotated_projection.y * resolution_y) / (rotated_projection.z / recording_screen_size.y) * self.camera.view_plane_z,
-            );
+
+        let mut x: f32 = -1.0;
+        let mut y: f32 = -1.0;
+
+        if rotated_projection.z != 0.0 {
+            x = (rotated_projection.x * (window_size.0 as f32)) / (rotated_projection.z / recording_screen_size.x) * self.camera.view_plane_z;
+            y = (rotated_projection.y * (window_size.1 as f32)) / (rotated_projection.z / recording_screen_size.y) * self.camera.view_plane_z;
         }
 
-        -Vector2D::ONE
+        (
+            x as i32 + (window_size.0 / 2) as i32,
+            y as i32 + (window_size.1 / 2) as i32,
+        )
     
+    }
+}
+
+#[derive(Default)]
+pub struct Transform {
+    pub position: Vector3D,
+    pub rotation: Vector3D,
+    pub scale: Vector3D
+}
+
+impl Transform {
+    pub fn new() -> Self {
+        Self {
+            position: Vector3D::ZERO,
+            rotation: Vector3D::ZERO,
+            scale: Vector3D::ONE
+        }
     }
 }
